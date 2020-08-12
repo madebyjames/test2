fb038322d91d3d08d9d2c6c34538a6e3ffb6d081
James Smith <8142061+madebyjames@users.noreply.github.com>
2020-08-12 21:42:28 +0100

    chore: headless mode

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,13 +55,15 @@ exports.config = {
             //
             browserName: 'chrome',
             acceptInsecureCerts: true
-            // If outputDir is provided WebdriverIO can capture driver session logs
-            // it is possible to configure which logTypes to include/exclude.
-            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-            // excludeDriverLogs: ['bugreport', 'server'],
+            'goog:chromeOptions': {
+                args: ['--headless', '--disable-gpu'],
+            }
         },
         {
             browserName: 'firefox',
+            'moz:firefoxOptions': {
+                args: ['-headless']
+            },
         }
     ],
     //
